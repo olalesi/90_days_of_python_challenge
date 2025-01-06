722a402f94b3a6eb39b118f33350fbf226918925
olalesi <horlahlehsih@gmail.com>
2025-01-06 11:22:30 +0100

    Add files via upload

---
+++ Python_Introduction_and_Setup.py
@@ -0,0 +1,8 @@
+# Print a greeting message
+print("Hello World")
+
+# Perform basic arithmetic and print the results
+print("2 + 2 =", 2 + 2)
+print("5 * 3 =", 5 * 3)
+print("10 - 4 =", 10 - 4)
+print("20 / 5 =", 20 / 5)
