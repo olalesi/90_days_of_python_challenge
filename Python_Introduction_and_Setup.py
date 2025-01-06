e0b8b3f88a545bf8ea5223604fa1e2c9aca346f7
olalesi <horlahlehsih@gmail.com>
2025-01-06 21:03:07 +0100

    Update Python_Introduction_and_Setup.py

--- Python_Introduction_and_Setup.py
+++ Python_Introduction_and_Setup.py
@@ -3,6 +3,6 @@ print("Hello World")
 
 # Perform basic arithmetic and print the results
 print("2 + 2 =", 2 + 2)
-print("5 * 3 =", 5 * 3)
-print("10 - 4 =", 10 - 4)
-print("20 / 5 =", 20 / 5)
+print("5 * 34 =", 5 * 34)
+print("5840 - 40 =", 5840 - 40)
+print("2000 / 50 =", 2000 / 50)
