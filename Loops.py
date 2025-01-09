ec07c888b00ddbd23e3c459ed0f013fe5f340628
olalesi <horlahlehsih@gmail.com>
2025-01-09 10:56:59 +0100

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,17 @@
+# Countdown Program Using a while loop
+
+count = 10
+while count > 0:
+  print(f"Countdown: {count}")
+  count -= 1
+
+print("Blast off!")
+
+
+
+# Countdown Program Using a for loop
+
+for count in range(10, 0, -1):
+  print(f"Countdown: {count}")
+
+print("Blast off!")
